b6a8f26e57f7bb3a53aacaacdd09e051835ebea6
kamblekunal430 <kamblekunal430@gmail.com>
2021-10-11 18:03:10 +0530

    created home component and used json server as a backend and successfully fetched the data from the json server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -17,7 +17,7 @@ import { FormsModule } from '@angular/forms';
     HomeComponent,
     NewblogComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  blogs: any = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
+    this.http.get('http://localhost:8000/blogs').subscribe(
+      (response: any) => {
+        console.log('Data fetched successfully\n', response);
+        this.blogs = response;
+      },
+      (error: any) => {
+        console.log('Error occured while fetching blogs\n', error);
+      }
+    );
   }
 
+  ngOnInit(): void {}
 }
