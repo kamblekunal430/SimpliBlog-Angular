9064d1d2b57c96306a0750455a15a7e67bd6af81
kamblekunal430 <kamblekunal430@gmail.com>
2021-10-11 18:33:35 +0530

    now the user can add blog using json server and show it on homepage

--- src/app/newblog/newblog.component.ts
+++ src/app/newblog/newblog.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,11 +9,25 @@ import { Component, OnInit } from '@angular/core';
 export class NewblogComponent implements OnInit {
   title: any;
   content: any;
-  constructor() {}
+  blog: any = {};
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
   addBlog() {
     console.log(this.title);
     console.log(this.content);
+    this.blog = {
+      title: this.title,
+      content: this.content,
+    };
+
+    this.http.post('http://localhost:8000/blogs', this.blog).subscribe(
+      (response: any) => {
+        console.log('blog added successfully', response);
+      },
+      (error: any) => {
+        console.log('error occured', error);
+      }
+    );
   }
 }
