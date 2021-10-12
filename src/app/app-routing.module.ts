f1f1e0c1dd24d9b2a3c7ed6df4ac72d698c038aa
kamblekunal430 <kamblekunal430@gmail.com>
2021-10-12 10:42:48 +0530

    now the user can view the individual blog through view blog component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { NewblogComponent } from './newblog/newblog.component';
+import { ViewBlogComponent } from './view-blog/view-blog.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'newBlog', component: NewblogComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'viewBlog/:id', component: ViewBlogComponent },
 ];
 
 @NgModule({
--- src/app/view-blog/view-blog.component.ts
+++ src/app/view-blog/view-blog.component.ts
@@ -1,15 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-blog',
   templateUrl: './view-blog.component.html',
-  styleUrls: ['./view-blog.component.css']
+  styleUrls: ['./view-blog.component.css'],
 })
 export class ViewBlogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  blogId: any;
+  blog: any;
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
+    this.route.params.subscribe((params) => (this.blogId = params.id));
   }
 
+  ngOnInit() {
+    this.http.get('http://localhost:8000/blogs/' + this.blogId).subscribe(
+      (response: any) => {
+        console.log('Blog with id ' + this.blogId + ' fetched', response);
+        this.blog = response;
+      },
+      (error: any) => {
+        console.log('cannot fetch blog');
+      }
+    );
+  }
 }
