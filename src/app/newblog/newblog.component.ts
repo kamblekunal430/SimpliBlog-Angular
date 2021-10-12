a65ace3a22ffe3669879574504c6e62516248b2e
kamblekunal430 <kamblekunal430@gmail.com>
2021-10-12 13:33:49 +0530

    now user can add images to the blog

--- src/app/newblog/newblog.component.ts
+++ src/app/newblog/newblog.component.ts
@@ -9,16 +9,36 @@ import { Component, OnInit } from '@angular/core';
 export class NewblogComponent implements OnInit {
   title: any;
   content: any;
+  author: any;
+  image: any;
   blog: any = {};
-  constructor(private http: HttpClient) {}
+  base64: any;
 
-  ngOnInit(): void {}
+  handleReaderLoaded = (readerEvent: any) => {
+    this.image = readerEvent.target.result;
+    console.log('2 ', this.image);
+  };
+
+  onImgUpload(event: any) {
+    let file = event.target.files[0];
+    console.log('file to upload', file);
+
+    // if file exist convert into base64 string
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = this.handleReaderLoaded.bind(this);
+      reader.readAsDataURL(file);
+    }
+  }
+
   addBlog() {
     console.log(this.title);
     console.log(this.content);
+    console.log('addblog', this.image);
     this.blog = {
       title: this.title,
       content: this.content,
+      image: this.image,
     };
 
     this.http.post('http://localhost:8000/blogs', this.blog).subscribe(
@@ -30,4 +50,7 @@ export class NewblogComponent implements OnInit {
       }
     );
   }
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {}
 }
